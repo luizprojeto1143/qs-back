0bf1b5e19dd4f83498f58ba1e1108552a84c4c4e
Luiz Alberto <luizqsinclusao@gmail.com>
2025-12-11 14:03:59 -0300

    Bump: Frontend version to v8 for deploy verification

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -43,7 +43,7 @@ import UniversityReports from './pages/university/UniversityReports';
 import Certificates from './pages/university/Certificates';
 
 function App() {
-  console.log('QS System Version: Libras Central (v7)');
+  console.log('QS System Version: Libras Central (v8)');
   return (
     <CompanyProvider>
       <Toaster position="top-right" richColors />
