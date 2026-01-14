23987ff8fbb12f2f79ceffbece4f9a4452f4c173
Luiz Alberto <luizqsinclusao@gmail.com>
2026-01-14 09:53:02 -0300

    fix: remove caracteres invalidos residuais em VisitDetailView

--- client/src/components/visits/VisitDetailView.tsx
+++ client/src/components/visits/VisitDetailView.tsx
@@ -112,8 +112,7 @@ export const VisitDetailView = ({ data, index }: VisitDetailViewProps) => {
                                 <p className="text-yellow-800">{note.content}</p>
                             </div>
                         ))}
-                        ))
-                            }
+
                     </div>
                 </div>
             )}
